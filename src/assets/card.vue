60b5ba6e20a8c152c625ae339c878e7f92c2cd1a
felipejardim <felipejardim95@gmail.com>
2020-02-17 16:16:08 -0400

    pequenas correcoes

--- src/assets/card.vue
+++ src/assets/card.vue
@@ -2,7 +2,7 @@
   <div class="card" >
           <label> {{titulo}} </label>
           <img class="img" v-if="aimg" :src="aimg" />
-          <p class="texto" v-else >Ops, houve um erro ao carregar a imagem</p>
+          <p class="texto" v-else >Ops, houve um erro ao carregar a imagem</p> <!-- msg para quando não se é possível carregar alguma img -->
       </div>
 </template>
 
@@ -14,7 +14,8 @@ props: {titulo: null, grafTipo: null, grafCurso: null, ano: null},
 
 data(){
     return{
-        
+        excecoes: [38, 40, 45, 49, 51, 54, 56, 64, 90, 98, 100, 102], //cursos que possuêm falta de imgs
+
     }
 },
 
@@ -62,8 +63,9 @@ computed: {
 }
 
 .card p{
-    background-color: lightslategrey;
+    background-color: #f2f2f2;
     font-weight: bold;
+    font-size: 14px; 
 
     padding: 20px;
     width: 160px;
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: '/beta/'
+  publicPath: '/infocursos/beta/',
+  
 }
